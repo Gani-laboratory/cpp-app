46bd7fdd3a9071129d0097cbab92a7ae48c06e3b
lelouch <lelouch@ll.ll.my.id>
2022-05-28 13:57:17 +0700

    methods

--- src/oop/main.cpp
+++ src/oop/main.cpp
@@ -12,20 +12,45 @@ class Mahasiswa {
 
     Mahasiswa(string inputNama, string NIM,string jurusan, double IPK) {
         // cout << "ini adalah constructor" << endl;
-        nama = inputNama;
+        Mahasiswa::nama = inputNama;
         Mahasiswa::NIM = NIM;
         Mahasiswa::jurusan = jurusan;
         Mahasiswa::IPK = IPK;
+    }
+
+    void showData() {
+        cout << "nama: " << nama << endl;
+        cout << "NIM: " << NIM << endl;
+        cout << "jurusan: " << jurusan << endl;
+        cout << "IPK: " << IPK << endl;
+    }
+
+    void editData(const char* newNama) {
+        Mahasiswa::nama = newNama;
+    }
+
+    string getNama() {
+        return nama;
+    }
+
+    double getIPK() {
+        return IPK;
+    }
+
+    double tambahIPK(const double &tambahanNilai) {
+        return IPK + tambahanNilai;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    Mahasiswa data1 = Mahasiswa("Gani", "0129307", "RPL", 9);
+    Mahasiswa data1 = Mahasiswa("Gani", "0129307", "RPL", 7.1);
     cout << "===== DATA 1 =====" << endl;
-    cout << "nama: " << data1.nama << endl;
-    cout << "NIM: " << data1.NIM << endl;
-    cout << "jurusan: " << data1.jurusan << endl;
-    cout << "IPK: " << data1.IPK << endl;
+    data1.showData();
+    data1.editData("Ardy");
+    cout << "Nama Baru: " << data1.getNama() << endl;
+    data1.tambahIPK(1);
+    cout << "IPK Baru: " << data1.getIPK() << endl;
+
     return 0;
 }
