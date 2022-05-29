416d6a4eee52ccf03745c06b9eed42917ab86912
Yukinoshita404 <freeacount404@gmail.com>
2022-05-29 11:33:48 +0700

    destructor & object lifetime

--- src/windows/memory.cpp
+++ src/windows/memory.cpp
@@ -7,10 +7,18 @@ class ExMemory
 {
   public:
   string data;
+  // constructor
   ExMemory(const char* args) {
     data = args;
+    cout << "memory " << data << " dibuat" << endl;
   }
 
+  // destructor
+  ~ExMemory() {
+    // memory address akan di hapus otomatis jika berada di memo stack
+    cout << "memory " << data << " dihapus" << endl;
+  }
+  // method
   void show() {
     cout << data << endl;
   }
@@ -19,14 +27,22 @@ class ExMemory
 int main(int argc, char const *argv[])
 {
   // membuat object di stack memory
-  ExMemory stackMemo("ini object di stack memory");
-  stackMemo.show();
+  {
+    ExMemory stackMemo("stack");
+  }
+  // stackMemo.show();
+
+  {
+    ExMemory* heapMemo = new ExMemory("heap");
+    // menghapus memo addr pada heap memory
+    delete heapMemo;
+  }
 
   // membuat object di heap / free memory
-  // ExMemory* heapMemo = new ExMemory("ini object di heap memory");
-  ExMemory* heapMemo;
-  heapMemo = new ExMemory("ini object di heap memory");
-  (*heapMemo).show(); // perlu di dereference
-  heapMemo->show(); // arrow operator mapping function and attribute dari pointer
+  // ExMemory* heapMemo;
+  // heapMemo = new ExMemory("ini object di heap memory");
+  // (*heapMemo).show(); // perlu di dereference
+  // heapMemo->show(); // arrow operator mapping function and attribute dari pointer
+
   return 0;
 }
