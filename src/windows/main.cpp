3733756d749c196e2dc70fae1526beb78258bac7
Yukinoshita404 <freeacount404@gmail.com>
2022-05-29 12:04:43 +0700

    namespace vs this

---
+++ src/windows/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class ClassTypes{
+  public:
+    string dataStr;
+    double health;
+
+    ClassTypes(const char* dataStr, double health){
+      this->dataStr = dataStr;
+      this->health = health;
+      // this = pointer dari object
+      cout << this->dataStr << endl;
+    }
+
+    // penggunaan namespace dalam class biasa digunakan untuk mendefinisikan sebuah prototype
+    void setHealth(int);
+};
+
+void ClassTypes::setHealth(int health){
+  this->health = health;
+}
+
+int main(int argc, char const *argv[])
+{
+  ClassTypes* obj = new ClassTypes("object 1", 1);
+  cout << obj->health << endl;
+  obj->setHealth(2);
+  cout << obj->health << endl;
+
+  return 0;
+}
